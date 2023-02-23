3b980e82df903466088cf99f7fe51c7bf9cff607
GERSON <Gersonponce6@gmail.com>
2023-02-23 14:35:01 -0600

    Buclefor

---
+++ Tareasemana5/Bucle for/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Digita el primer numero");
+String dato = Console.ReadLine();
+int a = Convert.ToInt32(dato);
+
+Console.WriteLine("Digita el segundo numero");
+String dato2 = Console.ReadLine();
+int datonum2 = Convert.ToInt32(dato2);
+
+for (int i = a; i < datonum2; i++)
+{
+    if (i % 2 == 0)
+    {
+     Console.WriteLine(i);
+    }
+   
+}
